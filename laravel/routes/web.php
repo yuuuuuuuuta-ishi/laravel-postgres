7c56812bb49373905741f6987d0e763ff53fc8e7
yuuuuuuuuta-ishi <0605ishihara@gmail.com>
2025-03-02 19:32:20 +0900

    fix vite config

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'greeting' => 'Welcome to Laravel Inertia App'
+    ]);
 });
--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -4,6 +4,12 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  server: {
+    host: true,
+    hmr: {
+      host: "localhost",
+    },
+  }, // 追記ここまで
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
